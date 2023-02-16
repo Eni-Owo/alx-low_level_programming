577278313c4ab2c87fae24acfc48b1ce392fbb09
Eni-Owo <olusegunakinwale13@gmail.com>
2023-02-16 08:30:20 -0800

    commit to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints using function puts
+ * Return: 0 (pass)
+ */
+int main(void)
+{
+puts(*\"Programming is like building a multilingual puzzle");
+return (0);
+}

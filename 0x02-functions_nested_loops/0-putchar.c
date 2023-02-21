124a0acd0f969044df8a86062d19034b78f434ec
Eni-Owo <olusegunakinwale13@gmail.com>
2023-02-21 21:37:43 +0100

    update 01

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,39 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: 0 (pass)
- */
-int main(void)
-{
-	char *sh = "putchar";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+        /**
 
-	return (0);
-}
+         * main - print _putchar
+
+         * is written by mbah
+
+         * Return: 0
+
+         */
+
+
+
+        int main(void)
+
+        {
+
+        _putchar('_');
+
+        _putchar('p');
+
+        _putchar('u');
+
+        _putchar('t');
+
+        _putchar('c');
+
+        _putchar('h');
+
+        _putchar('a');
+
+        _putchar('r');
+
+        _putchar('\n');
+
+        return (0);
+
+        }

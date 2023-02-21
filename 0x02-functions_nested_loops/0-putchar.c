056e62965746fdbfad0cf8a090add154a915740d
Eni-Owo <olusegunakinwale13@gmail.com>
2023-02-21 21:04:58 +0100

    commit 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 (pass)
+ */
+int main(void)
+{
+	char *sh = "putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}

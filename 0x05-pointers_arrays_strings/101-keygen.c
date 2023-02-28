2430bad4e74a3ac5e172818fc1bbb28a9caf8a2e
Eni-Owo <olusegunakinwale13@gmail.com>
2023-02-28 03:33:17 +0100

    fixing commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,32 +3,52 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- * Return: Always 0 (Pass)
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	sum = 0;	
+	srand(time(0));
 
-	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
+	while (sum < 2772)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
 		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
 		}
 	}
+
+	printf("%s", password);
 
 	return (0);
 }

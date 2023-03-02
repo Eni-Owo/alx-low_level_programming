b183421c3e516e88fccb17247e7dd0a4623acb8c
Eni-Owo <olusegunakinwale13@gmail.com>
2023-03-02 14:04:47 +0100

    fix 103

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ char *leet(char *n);
 char *rot13(char *n);
 void rev_string(char *n);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif

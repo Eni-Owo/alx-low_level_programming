288b771fec26e07c19865a49e55f2a1b83db8313
Eni-Owo <olusegunakinwale13@gmail.com>
2023-03-10 21:28:36 +0100

    add c files

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * @dest: copy to
+ * @src: copy from
+ * @n: input number of char
+ * Return: Always 0 (Pass)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+my readme file for 0x07 static libraries
